cfaa87388d03ec7bb73d7ce68577780d29f3763d
christiancostap <christiancosta.parreiras@yahoo.com.br>
2022-05-08 19:30:21 -0300

    codigo compilado em um arquivo utilizando @vercel/ncc

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,17 @@
+// To compile - npx ncc build .github/actions/hello/index.js -o .github/actions/hello/dist
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // throw (new Error('some error message'));
+
+  const name = core.getInput('who-to-greet');
+  console.log(`Hello ${name}`);
+
+  const time = new Date();
+  core.setOutput('time', time.toLocaleTimeString());
+
+  console.log(JSON.stringify(github, null, '\t'));
+} catch (error) {
+  core.setFailed(error.message);
+}
