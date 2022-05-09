36ae2e041f1eefc98d07b688f80f5d09b83d7990
christiancostap <christiancosta.parreiras@yahoo.com.br>
2022-05-09 09:05:02 -0300

    changing permissions on local files

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,7 +3,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  throw (new Error('some error message'));
+  // throw (new Error('some error message'));
+
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
 
   const name = core.getInput('who-to-greet');
   console.log(`Hello ${name}`);
